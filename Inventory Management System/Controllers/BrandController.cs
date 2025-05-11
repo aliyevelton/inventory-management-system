5179627312c445f5feff4c3e302487fea925877b
Elton Aliyev <aliyevelton88@gmail.com>
2025-05-12 00:43:19 +0400

    Brand/Category

---
+++ Inventory Management System/Controllers/BrandController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_Management_System.Controllers;
+
+public class BrandController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Inventory Management System/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_Management_System.Controllers;
+
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
